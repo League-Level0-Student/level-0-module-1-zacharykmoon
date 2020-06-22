c28d376ef88ffbaad4824f6f860585027e0de28a
root <root@1675c2445ab7>
2020-06-22 20:26:27 +0000

    module 1 start

--- src/_02_colors/VennDiagram/VennDiagram.pde
+++ src/_02_colors/VennDiagram/VennDiagram.pde
@@ -1,1 +1,2 @@
 size(600,600);
+JOptionPane.showMessageDialog(null, "happy birthday. You are nice friend");
--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,11 +8,11 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+		JOptionPane.showMessageDialog(null, "happy birthday. You are nice friend");
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+		String input = JOptionPane.showInputDialog("enter a name");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+		JOptionPane.showMessageDialog(null, " you are a nice friend");
 	}
 }
 
